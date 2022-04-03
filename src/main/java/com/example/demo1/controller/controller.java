cf9f1db0b9092578aecb912d154af87efb7af473
Gourav Sutradhar <gouravsutradhar@Gouravs-MacBook-Air.local>
2022-04-03 16:34:42 +0530

    init

---
+++ src/main/java/com/example/demo1/controller/controller.java
@@ -0,0 +1,53 @@
+package com.example.demo1.controller;
+
+
+import com.example.demo1.domain.Student;
+import com.example.demo1.service.studentservice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class controller {
+    @Autowired
+    private studentservice service;
+
+    @GetMapping("/")
+    public String viewHomePage(Model model) {
+        List<Student> liststudent = service.listAll();
+        model.addAttribute("liststudent", liststudent);
+        System.out.print("Get / ");
+        return "index";
+    }
+
+    @GetMapping("/new")
+    public String add(Model model) {
+        model.addAttribute("student", new Student());
+        System.out.print("Get / ");
+        return "new";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveStudent(@ModelAttribute("student") Student std) {
+        service.save(std);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditStudentPage(@PathVariable(name = "id") int id) {
+        ModelAndView mav = new ModelAndView("new");
+        Student std = service.get(id);
+        mav.addObject("student", std);
+        return mav;
+
+    }
+    @RequestMapping("/delete/{id}")
+    public String deletestudent(@PathVariable(name = "id") int id) {
+        service.delete(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/example/demo1/service/studentservice.java
@@ -0,0 +1,31 @@
+package com.example.demo1.service;
+
+import com.example.demo1.domain.Student;
+import com.example.demo1.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class studentservice {
+    @Autowired
+    private StudentRepository repo;
+
+    public List<Student> listAll() {
+        return repo.findAll();
+    }
+
+    public void save(Student std) {
+        repo.save(std);
+    }
+
+    public Student get(long id) {
+        return repo.findById(id).get();
+    }
+
+    public void delete(long id) {
+        repo.deleteById(id);
+    }
+
+}
